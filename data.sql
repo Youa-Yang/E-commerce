a139733fa8810d3d0354b0af4d9c1adda4838444
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-11-30 16:04:52 -0600

    added dummy data to test sql

---
+++ data.sql
@@ -0,0 +1,18 @@
+INSERT INTO category_t
+VALUE 
+(1,"Skirt"),
+(2,"Shirt"),
+(3,"Dress"),
+(4,"Pants"),
+(5,"Sweater");
+
+
+
+INSERT INTO Product_t
+VALUES 
+(01, "High-waisted mini skirt with elastic waistband.","S",50.25,"Blue", 15,123456,"Image01",1),
+(02, "Mid-rise pants with side pockets and back false welt pockets. 
+Front zip, metal hook, and interior button closure.","M",45.90,"Blue", 15,159753,"Image02",4),
+(03, "Petite recycled lace trim long sleeve mini dress","L",20.20,"Blue", 15,165412,"Image03",3),
+(04, "Strap detail slip dress","S",74.00,"Gold", 15,147258,"Image04",3),
+(05, "Full cut round neck sleeveless top","M",85.89,"Black", 15,258369,"Image05",2);
