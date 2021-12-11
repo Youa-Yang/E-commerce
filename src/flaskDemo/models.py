f65b2cf86b20694bf46ec8cb0dd15374af3e0e50
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-12-11 16:44:50 -0600

    init commit with carts

--- src/flaskDemo/models.py
+++ src/flaskDemo/models.py
@@ -1,62 +1,104 @@
-from datetime import datetime
-from flaskDemo import db, login_manager
-from flask_login import UserMixin
-from functools import partial
-from sqlalchemy import orm
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import backref
-
-db.Model.metadata.reflect(db.engine)
+from flaskDemo.models import Product_T,Category_T,Size_T,Color_T,Order_T
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField, DateField, SelectField, HiddenField,FloatField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskDemo.models import User
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+sizes = Size_T.query.with_entities(Size_T.SizeID,Size_T.Size);
+categories = Category_T.query.with_entities(Category_T.CategoryID, Category_T.CategoryType).distinct();
+products = Product_T.query.with_entities(Product_T.ProductID).distinct();
+orders = Order_T.query.with_entities(Order_T.OrderID).distinct();
 
+for row in sizes:
+    rowDict=row._asdict()
+    sizes_choice = [(row['SizeID'],(row['Size'])) for row in sizes]
 
-class User(db.Model, UserMixin):
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-
-
-class Customer_T(db.Model):
-    __table__ = db.Model.metadata.tables['customer_t']
-
-class Order_T(db.Model):
-    __table__ = db.Model.metadata.tables['order_t']
-
-class Product_T(db.Model):
-    __table__ = db.Model.metadata.tables['product_t']
-
-class OrderLine_T(db.Model):
-    __table__ = db.Model.metadata.tables['orderline_t']
-
-class BillingAddress_T(db.Model):
-    __table__ = db.Model.metadata.tables['billingaddress_t']
-
-class ShippingAddress_T(db.Model):
-    __table__ = db.Model.metadata.tables['shippingaddress_t']
+for row in categories :
+    rowDict=row._asdict()
+    categories_choice = [(row['CategoryID'],row['CategoryType']) for row in categories]
     
-class Category_T(db.Model):
-    __table__ = db.Model.metadata.tables['category_t']    
+for row in products:
+    rowDict=row._asdict()
+    products_choice = [(row['ProductID'],row['ProductID']) for row in products]
+    
+for row in orders:
+    rowDict=row._asdict()
+    orders_choice = [(row['OrderID'],row['OrderID']) for row in orders]    
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class ProductForm(FlaskForm):
+    productName = TextAreaField('Product Description', validators=[DataRequired()])
+    productColor = StringField('Product Color', validators=[DataRequired()])
+    image = FileField('Product Image', validators=[FileAllowed(['jpg', 'png'])])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    price = FloatField('Product price', validators=[DataRequired()])
+    size = SelectField("Size", choices=sizes_choice);
+    category = SelectField("Category", choices=categories_choice);    
+    submit = SubmitField('Add product')
+    
+class UpdateProductForm(FlaskForm):
+    productName = TextAreaField('Product Description', validators=[DataRequired()])
+    image = FileField('Product Image', validators=[FileAllowed(['jpg', 'png'])])
+    price = FloatField('Product price', validators=[DataRequired()]) 
+    submit = SubmitField('Update product')
+    
+class CartUpdateForm(FlaskForm):
+    orderId = IntegerField('OrderID', validators=[DataRequired()])
+    productId = SelectField("ProductID", choices= orders_choice)
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    submit = SubmitField('Update product')
+
+
+class AddToCartForm(CartUpdateForm):
+    submit = SubmitField('Add to Cart')    
